663ef692833794c71a60ee99d0a5ce76c4462a6a
Hemant Garg <hanygarg1452@gmail.com>
2024-08-20 19:06:14 +0530

    Manage diff. users

--- SpringSecurity/src/main/java/com/spring/security/Controller/SeccurityConfig.java
+++ SpringSecurity/src/main/java/com/spring/security/Controller/SeccurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,5 +22,21 @@ public class SeccurityConfig {
         //http.formLogin(withDefaults()); //For UI
         http.httpBasic(withDefaults()); //without UI.. so that we can use authC & authR in tools like PostMan
         return http.build();
+    }
+
+    //Manager users - we can manage more than 1 users to access the application.
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}pass1")
+                .roles("user")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}AdminPass")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 }

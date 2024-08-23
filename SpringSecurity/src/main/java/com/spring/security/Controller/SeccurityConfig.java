aab8c1e847d5606e284135197976f28d72f13a5e
Hemant Garg <hanygarg1452@gmail.com>
2024-08-23 15:58:55 +0530

    Database Connection

--- SpringSecurity/src/main/java/com/spring/security/Controller/SeccurityConfig.java
+++ SpringSecurity/src/main/java/com/spring/security/Controller/SeccurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring.security.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,13 +9,19 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SeccurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -37,6 +44,13 @@ public class SeccurityConfig {
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user1, admin);
+        // JdbcUserDetailsManager: this is use to create users in database
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+                userDetailsManager.createUser(user1);
+                userDetailsManager.createUser(admin);
+                return userDetailsManager;
+
+        // InMemoryUserDetailsManager: this is use to create users in memory not database
+//        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
